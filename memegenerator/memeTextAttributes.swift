983616b04b917f34112da7fc98ea528d2e03e212
Maciej Serwicki <macserwicki@gmail.com>
2016-07-24 23:40:56 -0700

    Image and text setup

---
+++ memegenerator/memeTextAttributes.swift
@@ -0,0 +1,21 @@
+//
+//  memeTextAttributes.swift
+//  memegenerator
+//
+//  Created by Maciej Serwicki on 7/24/16.
+//  Copyright © 2016 Maciej Serwicki. All rights reserved.
+//
+
+import UIKit
+
+class memeTextAttributes: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
